b77de38d2cad7bacea458e826893b0a003f68b02
Nemuroscythe <58225415+Nemuroscythe@users.noreply.github.com>
2023-04-29 22:57:16 +0200

    First commit: Creation d'un module Odoo

---
+++ id_card_reader_controller/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Controller for ID Card Reader",
+    "category": "User types",
+    "version": "15.0.0",
+    "author": "Jeremy Alen",
+    "website": "",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": False
+}
