e5a96c1236814da8f18a336cdd0b863bf00024fb
Nemuroscythe <58225415+Nemuroscythe@users.noreply.github.com>
2023-04-30 19:06:13 +0200

    ID4 Changement de la méthode GET à POST, car le body ne peut être vide
    Axios ne gère pas de body pour les requêtes GET (ce qui a du sens), mais dans le cas d'Odoo, si je précise le type Json, ça influence la réponse ET la requête ! La requête ne peut donc pas être vide, nous forçant à utiliser la méthode POST

--- id_card_reader_controller/controllers/id_card_controller.py
+++ id_card_reader_controller/controllers/id_card_controller.py
@@ -2,6 +2,6 @@ from odoo import http
 
 
 class IdCardController(http.Controller):
-    @http.route('/id_card_reader/test', type="json", auth='none', cors='*', method=['GET'])
+    @http.route('/id_card_reader/test', type="json", auth='none', cors='*', method=['POST'])
     def test(self, **kw):
         return "Hello World"
