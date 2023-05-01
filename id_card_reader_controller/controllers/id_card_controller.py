374cde0e42d60f4ef8e21f37066a3157290eae0e
Nemuroscythe <58225415+Nemuroscythe@users.noreply.github.com>
2023-05-01 18:22:04 +0200

    ID9 Ajout d'une route pour la lecture de la carte retournant les données tel quels

--- id_card_reader_controller/controllers/id_card_controller.py
+++ id_card_reader_controller/controllers/id_card_controller.py
@@ -1,3 +1,5 @@
+import json
+
 from odoo import http
 
 
@@ -5,3 +7,8 @@ class IdCardController(http.Controller):
     @http.route('/id_card_reader/test', type="json", auth='none', cors='*', method=['POST'])
     def test(self, **kw):
         return "Hello World"
+
+    @http.route('/id_card_reader/', type="json", auth='none', cors='*', method=['POST'])
+    def test(self, **kw):
+        body = json.loads(http.request.httprequest.data)
+        return body["result"]
